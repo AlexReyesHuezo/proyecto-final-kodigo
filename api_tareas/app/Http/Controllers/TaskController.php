81b1d8d4297894c796ab6e4cd431b75909020362
Alex Reyes <73624939+AlexReyesHuezo@users.noreply.github.com>
2024-07-24 16:25:49 -0600

    mejore el index() para que solo accedan usuarios autenticados y el store() con retorno

--- api_tareas/app/Http/Controllers/TaskController.php
+++ api_tareas/app/Http/Controllers/TaskController.php
@@ -6,6 +6,7 @@ use Illuminate\Database\QueryException;
 use Exception;
 use App\Models\Task;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TaskController extends Controller
 {
@@ -14,7 +15,7 @@ class TaskController extends Controller
      */
     public function index()
     {
-        $tasks = Task::all();
+        $tasks = Task::where('user_id', Auth::id())->get();
         // Return the tasks in JSON format
         return response()->json($tasks);
     }
@@ -35,13 +36,23 @@ class TaskController extends Controller
         // Validate the request data
         try
         {
-            Task::create([
-                'title' => $request->title,
-                'description' => $request->description,
-                'completed' => $request->completed,
-                // Get the user ID from the authenticated user
-                'user_id' => auth()->user()->id
+            $validatedData = $request->validate([
+                'title' => 'required|string|max:100',
+                'description' => 'required|string',
+                'completed' => 'required|boolean'
             ]);
+
+            Task::create([
+                'title' => $validatedData['title'],
+                'description' => $validatedData['description'],
+                'completed' => $validatedData['completed'],
+                // Get the user ID from the authenticated user
+                'user_id' => Auth::id()
+            ]);
+
+            return response()->json([
+                'message' => 'Tarea guardada correctamente.'
+            ], 201);
         }
         // Catch the QueryException that could be thrown when trying to save the task
         catch (QueryException $error){
