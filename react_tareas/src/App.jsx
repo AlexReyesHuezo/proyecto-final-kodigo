683760c970c57634de9f85a12ce521e3ba7e318f
ElGzus <jeslopz00@gmail.com>
2024-07-25 14:50:16 -0600

    feat: agrego instalaciones de enrutamiento router de react, y agrego carpetas necesarias para funcionalidad y vistas

--- react_tareas/src/App.jsx
+++ react_tareas/src/App.jsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Welcome } from './principal/Welcome';
+import { Homepage } from './principal/Homepage';
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"></link>
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route index path='/' element={<Welcome />} />
+          <Route path='/src/principal/HomePage.jsx' element={<Homepage />} />
+          <Route path='/src/taskList/taskList.jsx' element={<TaskList />} />
+          <Route path='/src/taskAdmin/taskAdmin.jsx' element={<TaskAdmin />} />
+        </Routes>
+      </BrowserRouter>
+
     </>
   )
 }
